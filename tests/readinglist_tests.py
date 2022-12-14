b5e011d3e3b4bae5df93c5f028715bfdc11598fd
Gary Benson <gary@gbenson.net>
2022-12-14 23:34:41 +0000

    tests/readinglist_tests.py: Add testcase for a broken entry

--- tests/readinglist_tests.py
+++ tests/readinglist_tests.py
@@ -80,6 +80,28 @@ E,oZ1FCAWpAISuK/PA*@@;0O08RuCMFD5iB3ZoS^4Yo)FD`od$EH=9>;FO&H==.WC<(0ng-?
 
          ),
 
+        ("URL+body Wikipedia link, with markup",
+         r'''
+6t(1K3ZqL8A1SiX1a#1t@j!N\1,9t+1HI<G3\i]<.kiY20F\@YEc5eU+@fj\Gp#FbDKB`6+?
+X:FEd9o_@V'FuDf.1FATU*F$<)(VC1Ums3Znk=<HD_l/O=#\DKIo^9.`.H9jqaP+D,P4+@0m
+UEc5Z&%15g@F)tc&AM$JA3ZpOD5s[Fu1c76E0eu-T/Mop_5qPA_3&E3I2`X8`2)R<[0L\GrB
+4>FiF)PqKDImoR%15g$9gpX7ATDj+Df.TY0eP-h$:A`LFCf?3/Q@"7ANCrUAU&;ME,8rsDEA
+:7+Cf(nEcYf64`tjY/N=1H6Z6jaASuTA<,uDbF(T!(/OaPeDe*R"B0%/TF`2A5A1_b@Bl8$$
+@VfTb$<SlQ3Gi2=Cb84hASuU(FEoni+`':u1,1+o4YS4&F$3>t77KjN->#D?79EM9E'6'8-?
+21D:JO=f$4R>UFEDJC3\N-tDETaDD*9XCGA1i,E+NQo@6HA7DfTJDGA1i,02Q(nATMs-DJ<N
+s?Ya4bAncO%BFP:X$;Fr>AKY])+Br&@AR][uDI$O21.<<JEb/iHH[B7:FCcS4ATr3CFD5i5A
+N_h;<+ohc9OW!a+A69XAncO%BHTcQ@ruX0Gp#[A%16tq:gn0OF*),6AKZACEbmlp6tg[aE&o
+X*GB\6o1.?.jCh7[0Bl7L!Df092DfRur7;m3Z06;)HE%c9OBlH3j7;upZ5@JSSBfun4@r,^2
+1c@'71.EC6GAhM;4YoK&@;KLrFD5W(+AHclBln96+FI^.FCdTk6o$SA8p,#_+>PW*0HbacEb
+m=KFCB96F$2<IA7]14%16VgHsq&*BPDX$/Tl)M4YoK>FEDJC3\N.1AR[eX7:C4HBPDX$4YfH
+EEb@%LAR[;J0JP:93ANNI1,LpE2_SidFDs8o06_Va4Uh`]1.?.jCh7[0Bl7L!Df092DfRur7
+;m3Z06;)HE%c9OBlH3j7;upZ5@JSSBk(sj0f1dC,r.q4Hsq&*BPDX$/S&:3AN_h;1,h?*$6W
+8YE-,Z.@V'R&1,(F;+FSZ#061W94o''',
+
+         '{{at|Wed, 14 Dec 2022 03:30:29 +0000}} '
+
+         ),
+
         ("URL+subject Instagram link",
          r'''
 6t(1K3Zpn4Be1A\3?U_$@j!N\1,9t,0KLmA3]/T6.kiY20F\@YEc5eU+@fj\Gp#FbDKB`6+?
