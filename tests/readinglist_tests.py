2c9dd16e772ba03da40b67ca5572a8861154775f
Gary Benson <gary@gbenson.net>
2023-01-24 11:04:05 +0000

    tests/readinglist_tests.py: Reinstate two incomplete testcases

--- tests/readinglist_tests.py
+++ tests/readinglist_tests.py
@@ -81,6 +81,30 @@ F\@VDf0Z.DKII0H#R=U+EV1>F>%TL@;0U@%144fBOPq&ATU(XFCm*a%15I@DKKH-F=gI4@;^
 
          ),
 
+        ("URL+body generic link",
+         r'''
+6t(1K3Zq.8DCcnb1*Atr@j!N\1,9t-1-.0L3\WK8.kiY20F\@YEc5eU+@fj\Gp#FbDKB`6+?
+X:FEd9o_@V'FuDf.1FATU*F$<)(VC1Ums3Znk=<HD_l/O=#\DKIo^9.`.H9jqaP+D,P4+@0m
+UEc5Z&%15g@F)tc&AM$JA3ZpO=5rCD`0LS5Z2F]MZ/Mo.=3%[-L6q/ae3(>_m7RKX%3(,\jB
+4>FiF)PqKDImoR%15g$9gpX7ATDj+Df.TY0eP-h$:A`LFCf?3/Q@"7ANCrUAU&;ME,8rsDEA
+:7+Cf(nEcYf64`tjY/N=1H6Z6jaASuTA<,uDbF(T!(/OaPeDe*R"B0%/TF`2A5A1_b@Bl8$$
+@VfTb$<SlQ3Gi2=Cb84hASuU(FEoni+`':u0Jk+p4YS4&F$3>t77KjN->#D?79EM9E'5p12^
+WN-:/=VR%13OOBQS?8F#ks-GB\6o1.?:uAS6-oEb&lmDETaD@rH3;@rH4$ASuU(F(TH(AM5e
+m1,:LnARdAJ1,!'RFD52uDff]'F"Ls9B6,Xf%16QbBlmctDCoR?@rH7.ASu3nDI[60F*2G@C
+j@URBOu"'ATKF4$4R>H6#:U\/6r\lDf03+Bl7Q+A8-+*F`;G:Df-\>D]in*DKTQ"@ruj6Bl7
+Q7+E1b0FD56-Cgh@#Gp$g=+@BgVDIFYN75@"c0KhEP+EqL5@q\"7$6Wqh@qB^(FD,5.E,ol<
+E+NO$+E(j7@3B6+B4W2QDg#i*+?`.K4ZG,Y3C@A&BlkJA@<Q3F75@"c0KhEa+CT=6BlkJ+@U
+X.b4Uha:DK?q@ATMs6Bk)7!Df0V=Bl5&8BOr;u@:NjkDe:,6BOr;^D/!L#Df-\@@<-'jDKI!
+a75@"c0KhEP+CT;%+CT)&+Dbb-ASaL=$@*b9D]i_%G%De3DJs$#/T5MLAS,Oc@rucT1.<<bB
+Or;pB4W3(Aftf*@rH7.ASu3nDI[6/+DG^9F(fK4F=\PKF!,[@FD)e>4Uha>+D,P4+CQC:BPD
+9o+F.O?4YfG''',
+
+         '{{at|Mon, 12 Dec 2022 22:27:07 +0000}} '
+         'https://www.theguardian.com/commentisfree/2022/dec/12/antibio'
+         'tics-eggs-britain-inconvenience-supply-shelves-'
+
+         ),
+
         ("URL-only Wikipedia link",
          r'''
 6t(1K3Zq.8DCcnc3$:t7Fs&Oo1,9t,2```K3\`?3.kiY20F\@YEc5eU+@fj\Gp#FbDKB`6+?
@@ -110,6 +134,28 @@ E,oZ1FCAWpAISuK/PA*@@;0O08RuCMFD5iB3ZoS^4Yo)FD`od$EH=9>;FO&H==.WC<(0ng-?
 
          '{{at|Sat, 10 Dec 2022 10:05:35 +0000}} '
          "[[wikipedia:Gunther]] ''<q>Gunnar</q>''"
+
+         ),
+
+        ("URL+body Wikipedia link, with markup",
+         r'''
+6t(1K3ZqL8A1SiX1a#1t@j!N\1,9t+1HI<G3\i]<.kiY20F\@YEc5eU+@fj\Gp#FbDKB`6+?
+X:FEd9o_@V'FuDf.1FATU*F$<)(VC1Ums3Znk=<HD_l/O=#\DKIo^9.`.H9jqaP+D,P4+@0m
+UEc5Z&%15g@F)tc&AM$JA3ZpOD5s[Fu1c76E0eu-T/Mop_5qPA_3&E3I2`X8`2)R<[0L\GrB
+4>FiF)PqKDImoR%15g$9gpX7ATDj+Df.TY0eP-h$:A`LFCf?3/Q@"7ANCrUAU&;ME,8rsDEA
+:7+Cf(nEcYf64`tjY/N=1H6Z6jaASuTA<,uDbF(T!(/OaPeDe*R"B0%/TF`2A5A1_b@Bl8$$
+@VfTb$<SlQ3Gi2=Cb84hASuU(FEoni+`':u1,1+o4YS4&F$3>t77KjN->#D?79EM9E'6'8-?
+21D:JO=f$4R>UFEDJC3\N-tDETaDD*9XCGA1i,E+NQo@6HA7DfTJDGA1i,02Q(nATMs-DJ<N
+s?Ya4bAncO%BFP:X$;Fr>AKY])+Br&@AR][uDI$O21.<<JEb/iHH[B7:FCcS4ATr3CFD5i5A
+N_h;<+ohc9OW!a+A69XAncO%BHTcQ@ruX0Gp#[A%16tq:gn0OF*),6AKZACEbmlp6tg[aE&o
+X*GB\6o1.?.jCh7[0Bl7L!Df092DfRur7;m3Z06;)HE%c9OBlH3j7;upZ5@JSSBfun4@r,^2
+1c@'71.EC6GAhM;4YoK&@;KLrFD5W(+AHclBln96+FI^.FCdTk6o$SA8p,#_+>PW*0HbacEb
+m=KFCB96F$2<IA7]14%16VgHsq&*BPDX$/Tl)M4YoK>FEDJC3\N.1AR[eX7:C4HBPDX$4YfH
+EEb@%LAR[;J0JP:93ANNI1,LpE2_SidFDs8o06_Va4Uh`]1.?.jCh7[0Bl7L!Df092DfRur7
+;m3Z06;)HE%c9OBlH3j7;upZ5@JSSBk(sj0f1dC,r.q4Hsq&*BPDX$/S&:3AN_h;1,h?*$6W
+8YE-,Z.@V'R&1,(F;+FSZ#061W94o''',
+
+         '{{at|Wed, 14 Dec 2022 03:30:29 +0000}} '
 
          ),
 
